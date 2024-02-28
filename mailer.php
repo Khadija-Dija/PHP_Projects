651b9aa97379027717b3faaceb9ca3766404fdd8
Khadija-Dija <108152053+Khadija-Dija@users.noreply.github.com>
2024-02-28 20:21:24 +0100

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -16,8 +16,8 @@ function forgot_pasword_reset($email,$token){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'ktraibi877@gmail.com';                     //SMTP username
-        $mail->Password   = 'mmoc fold jojr fuxx';                               //SMTP password
+        $mail->Username   = 'exemple@gmail.com';                     //SMTP username
+        $mail->Password   = '******';                               //SMTP password
         $mail->SMTPSecure = "tls";                              //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
